340acedf36a7e4028e4e8a009caa759843c502d2
raitztiago1 <raitztiago1@gmail.com>
2026-02-23 12:04:38 -0300

    Initialize project structure with Maven, including wrapper scripts and configuration files

---
+++ src/main/java/com/felicioecavalaro/gestao_aluguel/GestaoAluguelApplication.java
@@ -0,0 +1,13 @@
+package com.felicioecavalaro.gestao_aluguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAluguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAluguelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felicioecavalaro/gestao_aluguel/GestaoAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felicioecavalaro.gestao_aluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
