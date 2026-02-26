c41eeb424401948f3be913f33b2f47037b49ace5
raitztiago1 <raitztiago1@gmail.com>
2026-02-26 18:28:39 -0300

    refactor: Remove unused import statements from domain classes

--- src/main/java/com/felicioecavalaro/gestao_aluguel/domain/Sala.java
+++ src/main/java/com/felicioecavalaro/gestao_aluguel/domain/Sala.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,9 +13,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
